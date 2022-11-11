628a30af321730e02e8987b4909319ae94661dc6
fahim-fardin-1045 <fahimfardin1045@gmail.com>
2022-11-11 23:59:56 +0600

    front page

---
+++ movie ticket booking.c
@@ -0,0 +1,28 @@
+#include <windows.h>
+#include<stdio.h>
+#include<time.h>
+
+void enterchoice(void);
+int main(){
+enterchoice();
+return 0;
+}
+COORD coord = {0, 0};
+void gotoxy (int x, int y)
+{
+        coord.X = x; coord.Y = y;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+void enterchoice(void)
+{
+	int choice;
+	printf("                 Welcome To Movie Ticket Booking System\n");
+	printf(" ************#######************\n");
+	printf("             1- Admin           \n");
+	printf("             2- User          \n");
+	printf("             3- Exit system:                                   \n");
+	printf("**********************\n");
+	printf("  Enter your choice: ");
+	scanf("%d",&choice);
+	return choice;
+}
